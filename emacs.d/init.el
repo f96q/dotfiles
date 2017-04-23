0652e7a12469154d1ea10dd7442e4e22723e195f
danny <danny.dev8@gmail.com>
2017-04-23 09:26:30 +0900

    update init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -41,3 +41,17 @@
 (require 'init-loader)
 (setq init-loader-show-log-after-init 'error-only)
 (init-loader-load)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (elm-mode scss-mode sass-mode haml-mode yaml-mode elixir-mode go-mode coffee-mode js3-mode swift-mode guru-mode wgrep-ag wgrep ag auto-save-buffers-enhanced init-loader))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
