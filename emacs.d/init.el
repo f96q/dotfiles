e91f906bb587fa5ff4e1e138122e795d8c280893
danny <98850+f96q@users.noreply.github.com>
2023-07-15 13:44:36 +0900

    Refacter init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -41,8 +41,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (elm-mode scss-mode sass-mode haml-mode yaml-mode elixir-mode go-mode coffee-mode js3-mode swift-mode guru-mode wgrep-ag wgrep ag auto-save-buffers-enhanced init-loader))))
+   '(elm-mode scss-mode sass-mode haml-mode yaml-mode elixir-mode go-mode coffee-mode js3-mode swift-mode guru-mode wgrep-ag wgrep ag auto-save-buffers-enhanced init-loader)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
